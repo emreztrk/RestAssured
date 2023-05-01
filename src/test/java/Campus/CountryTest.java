0071bd9f61171f1f028c82d824ff178f540be692
emreztrk <emreozturk0717@gmail.com>
2023-05-01 21:28:16 +0300

    Jenkins error test

--- src/test/java/Campus/CountryTest.java
+++ src/test/java/Campus/CountryTest.java
@@ -152,7 +152,7 @@ public class CountryTest {
 
                 .then()
                 .log().body() // gelen body yi log olarak göster
-                .statusCode(400)
+                .statusCode(500) // jenkins için hatalı test yapıldı. normalde 400
                 .body("message",equalTo("Country not found"))
         ;
     }
